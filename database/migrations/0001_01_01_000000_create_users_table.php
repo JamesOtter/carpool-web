8b8b8d1d486e713605ca351ccf37e071d3f9d72b
JamesOtter <jamestan21403@gmail.com>
2025-01-13 22:30:46 +0800

    updated request and offers table foreign key data type

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,10 +13,12 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->integer('preference_id');
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->integer('contact_number');
             $table->rememberToken();
             $table->timestamps();
         });
